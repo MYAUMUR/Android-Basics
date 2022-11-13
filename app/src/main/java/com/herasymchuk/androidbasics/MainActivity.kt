f62f80a97c9d85eace272046b72e8d3ead7e68ec
Pavlo Herasymchuk <pavloherasumchuc@gmail.com>
2022-11-13 19:16:24 +0200

    More design changes App now use MD elements

--- app/src/main/java/com/herasymchuk/androidbasics/MainActivity.kt
+++ app/src/main/java/com/herasymchuk/androidbasics/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.herasymchuk.androidbasics
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.herasymchuk.androidbasics.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -12,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { v, keyCode, _ ->
+            handleKeyEvent(v, keyCode)
+        }
     }
 
     private fun calculateTip() {
@@ -39,5 +46,16 @@ class MainActivity : AppCompatActivity() {
     private fun formatAndDisplayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            calculateTip()
+            return true
+        }
+        return false
     }
 }
