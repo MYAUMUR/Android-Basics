cd722435b7ff8db1210bff6583afc6364434f524
Pavlo Herasymchuk <pavloherasumchuc@gmail.com>
2022-12-19 16:08:57 +0200

    Working on adapter

---
+++ affirmations2/src/main/java/com/herasymchuk/affirmations2/adapter/ItemAdapter.kt
@@ -0,0 +1,29 @@
+package com.herasymchuk.affirmations2.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.herasymchuk.affirmations2.R
+import com.herasymchuk.affirmations2.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) :
+    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = dataset.size
+}
